f87f729daa6a5d7f5e5ee20de2ee8aaac1257321
Jesus Mancilla <mancilla.jesus00@gmail.com>
2016-08-06 19:34:03 -0700

    Initial Commit

---
+++ SpotMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotMe
+//
+//  Created by Jesus Mancilla on 8/6/16.
+//  Copyright © 2016 Jesus Mancilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
