4f346ee40ae565bdf38355089dfac22aea1637fa
Ricky Avina <rickyavina@icloud.com>
2016-08-21 10:42:51 -0700

    Initial Commit

---
+++ SpotMe/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SpotMe
+//
+//  Created by Jesus Mancilla on 8/6/16.
+//  Copyright © 2016 Jesus Mancilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var feedTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        return UITableViewCell();
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
